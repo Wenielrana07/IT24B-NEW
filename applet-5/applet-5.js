dbec589dcd4294e068a9ee5385422be1edae4004
Wenielrana07 <rana.weniel@gmail.com>
2024-10-17 13:43:09 +0800

    add extender

--- applet-5/applet-5.js
+++ applet-5/applet-5.js
@@ -31,5 +31,20 @@ class weatherapp {
     }
 }
 
+class WeatherService extends WeatherApp {
+    async fetchWeather() {
+        const city = this.cityInput.value.trim();
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+    }
+
 
 }
