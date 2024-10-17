8cc6b0b4f23787b60555e0d75cf21190fbc744a1
Wenielrana07 <rana.weniel@gmail.com>
2024-10-17 13:40:45 +0800

    add constructor

--- applet-5/applet-5.js
+++ applet-5/applet-5.js
@@ -1,3 +1,17 @@
 class weatherapp {
-    
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+       
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+        this.getLocationBtn = document.getElementById('getLocationBtn');
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+
+        this.setupEventListeners();
+    }
 }
