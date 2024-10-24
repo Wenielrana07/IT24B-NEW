ddb08296aa54e2bb8fa91492970d72dda809715e
Wenielrana07 <rana.weniel@gmail.com>
2024-10-24 11:03:28 +0800

    add condition2

--- applet-6/applet-6.js
+++ applet-6/applet-6.js
@@ -6,4 +6,16 @@ document.getElementById('getWeather').addEventListener('click', function() {
         alert('Please enter both API Key and City name.');
         return;
     }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        .then(response => {
+            if (!response.ok) throw new Error('City not found');
+            return response.json();
+        })
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            alert(error.message);
+        });
 });
