48077eae127b911a94e46b030fec08d49c5c021b
Wenielrana07 <rana.weniel@gmail.com>
2024-10-24 11:02:34 +0800

    add element

---
+++ applet-6/applet-6.js
@@ -0,0 +1,4 @@
+document.getElementById('getWeather').addEventListener('click', function() {
+    const apiKey = document.getElementById('apiKeyInput').value;
+    const city = document.getElementById('city').value;
+});
